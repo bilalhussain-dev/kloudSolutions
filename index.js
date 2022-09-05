015f88c5ab351e3db3e4b2c100affba806fca0b5
appsDev <bilalhussian785@gmail.com>
2022-09-05 17:41:30 +0500

    modified app

--- index.js
+++
@@ -1,7 +0,0 @@
-// if("serviceWorker" in navigator) {
-//     navigator.serviceWorker.register("sw.js").then(register => {
-//         console.log("Registers", register)
-//     }).catch(err => {
-//         console.log('Failed')
-//     })
-// }
